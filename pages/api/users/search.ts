e5f941254b9da3916a6dd2194aa4108230978157
miga <migaa38@gmail.com>
2024-09-03 23:56:52 +0200

    search users

---
+++ pages/api/users/search.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '@/lib/prisma';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { username } = req.query;
+  // Vérification si le paramètre 'username' est fourni
+  if (!username || typeof username !== 'string') {
+    return res.status(400).json({ error: 'Username query parameter is required' });
+  }
+
+  try {
+    // Recherche des utilisateurs dont le nom commence par le 'username' fourni
+    const users = await prisma.user.findMany({
+      where: {
+        username: {
+          startsWith: username,
+          mode: 'insensitive', // Recherche insensible à la casse
+        },
+      },
+      select: {
+        username: true, 
+      },
+    });
+
+    return res.status(200).json(users);
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,13 +1,46 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Form, FormField, FormItem, FormMessage, useZodForm } from "@/components/ui/form";
 import { Input } from '@/components/ui/input';
 
-export default async function Search() {
+export default function Search() {
     const [searchBar, setsearchBar] = useState()
-  return (
+    const [users, setUsers] = useState<any[]>([]); // Typage correct pour les utilisateurs
+    console.log("HERE");
+    
+    useEffect(() => {
+        if (!searchBar) return;
+        const fetchUsers = async () => {
+            try {
+                const response = await fetch(`/api/users/search?username=${searchBar}`);
+                if (response.ok) {
+                const data = await response.json();
+                setUsers(data);
+              } else {
+                console.error('Failed to fetch users:', response.statusText);
+              }
+            } catch (error) {
+              console.error('Error fetching users:', error);
+            }
+          };
+      
+          fetchUsers();
+    },[searchBar])
+
+    return (
     <div>
-        <Input  />
+        <Input onChange={(e) => {setsearchBar(e.target.value)}}/>
+        <ul>
+            {users.length > 0 ? (
+                users.map((user) => (
+                <li key={user.id}>
+                    {user.username} - {user.name}
+                </li>
+                ))
+            ) : (
+                <li>No users found</li>
+            )}
+        </ul>
     </div>
   )
 }
