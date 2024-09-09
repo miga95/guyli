918863cc1cec60431e7121c1ed27e0f1cabe7e71
miga <migaa38@gmail.com>
2024-09-09 17:23:05 +0200

    lifting state like

--- src/app/components/post/LikeButton.tsx
+++ src/app/components/post/LikeButton.tsx
@@ -1,41 +1,21 @@
 'use client';
 
-import { useState, useEffect, useTransition } from 'react';
 import clsx from 'clsx';
-import Loader from '../ui/loader';
 import { Heart } from 'lucide-react';
 
-export const LikeButton = ({postId, userId, isLikedInitial,}: {
-    postId: string;
-    userId: string;
-    isLikedInitial: boolean;
+export const LikeButton = ({ isLiked, onClick }: {
+    isLiked: boolean,
+    onClick: () => void,
 }) => {
-    const [isLiked, setIsLiked] = useState(isLikedInitial);
-
-    const updateLikeStatus = async () => {
-        try {
-            await fetch('/api/posts/like', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ postId, userId }),
-            });
-            setIsLiked(!isLiked);
-        } catch (error) {
-            console.error('Error liking the post:', error);
-            setIsLiked(isLiked);
-        }
-    };
-
     return (
         <button
             className={clsx('rounded-md hover:bg-accent flex gap-1 items-center', {
                 'text-red-500': isLiked,
             })}
-            onClick={updateLikeStatus}
+            onClick={onClick}
         >
-            {<Heart size={20} />}
+            <Heart size={20} />
         </button>
     );
 };
+
