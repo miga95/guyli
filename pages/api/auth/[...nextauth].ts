65f5ece56aaa0c3a7fedce1a74c113554c7a1759
miga <migaa38@gmail.com>
2024-09-10 20:06:37 +0200

    add id when getting user with useSession

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -9,8 +9,36 @@ import { PrismaClient } from '@prisma/client';
 
 import prisma from "@/lib/prisma";
 
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id?: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    }
+  }
+
+  interface User {
+    id: string;
+  }
+}
+
 export const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
+  callbacks: {
+    session: async ({ session, token }) => {
+      if (session?.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+    jwt: async ({ user, token }) => {
+      if (user) {
+        token.uid = user.id;
+      }
+      return token;
+    },
+  },
   session: {
     strategy: "jwt"
   },
